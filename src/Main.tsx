0c4759f1eb6271fb63deba9d5fbc10c8794781cf
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-11-25 12:51:52 -0600

    fix(config): loads default theme instead of requesting it

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,17 +1,13 @@
 import { MotionProvider } from '@kurocado-studio/systemhaus-motion-react';
 import {
-  FullScreenLoader,
   ThemeProvider,
+  defaultSystemHausThemeVariables,
 } from '@kurocado-studio/systemhaus-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '../tailwind.css';
 import { Demo } from './Demo';
-import {
-  KUROCADO_STUDIO_DEFAULT_THEME_ID,
-  KUROCADO_STUDIO_ORGANIZATION_ID_SYSTEMHAUS,
-} from './config/constants';
 
 const rootElement = document.querySelector('#root');
 
@@ -21,12 +17,10 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <ThemeProvider
-        LoaderComponent={FullScreenLoader}
         progressOptions={{
           minimumDelay: 150,
         }}
-        organizationId={KUROCADO_STUDIO_ORGANIZATION_ID_SYSTEMHAUS}
-        themeId={KUROCADO_STUDIO_DEFAULT_THEME_ID}
+        theme={defaultSystemHausThemeVariables}
       >
         <MotionProvider>
           <main className='h-screen w-screen space-y-2 overflow-y-auto bg-gray-100 py-8'>
